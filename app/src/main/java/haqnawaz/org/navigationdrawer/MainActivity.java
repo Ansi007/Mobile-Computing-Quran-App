8ae3eaa017bf4ca2583b3e24a16c7d95e4a7a833
Anns007 <annsshahbaz@gmail.com>
2022-09-07 23:33:06 +0500

    All tested, working fine, last commit

--- app/src/main/java/haqnawaz/org/navigationdrawer/MainActivity.java
+++ app/src/main/java/haqnawaz/org/navigationdrawer/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
